aeed85734aac59af0271484d621cd4481d733485
David Carlos <ddavidcarlos1392@gmail.com>
2018-06-04 18:50:29 -0300

    Minor fix on build script

--- scripts/build.rb
+++ scripts/build.rb
@@ -1,17 +1,18 @@
 #!/usr/bin/ruby
 
 def build(project)
-  return "informe um projeto para fazer a build" unless project
+return "informe um projeto para fazer a build" unless project
 
-  system("rm -rf #{project}/build")
-  system("rm -rf #{project}/dev")
+system("rm -rf #{project}/build")
+system("rm -rf #{project}/dev")
 
-  system("tar -xf #{project}/build.tar.gz")
-  system("rm #{project}/build.tar.gz")
-  system("mv build #{project}/dev")
-  system("mv #{project}/dev/unbundled #{project}/dev/build")
+system("(cd #{project} && tar -xf build.tar.gz)")
+system("rm #{project}/build.tar.gz")
+system("mv #{project}/build #{project}/dev")
+system("mv #{project}/dev/unbundled #{project}/dev/build")
 
-  system("sudo systemctl restart nginx")
+
+system("sudo systemctl restart nginx")
 
 end
 
